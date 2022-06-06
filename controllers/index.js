c086029bcb3cb74a83bfa704defe12f9662a1392
Neeraj Bharti <nbharti1@my.centennialcollege.ca>
2022-06-06 18:57:47 -0400

    My first asssignment

---
+++ controllers/index.js
@@ -0,0 +1,24 @@
+/* Getting the  controller of home page */
+exports.home = function (req, res) {
+  res.render("index", { title: "Home" });
+};
+
+/* GET projects page. controller*/
+exports.projects = function (req, res) {
+  res.render("projects", { title: "Projects" });
+};
+
+/* GET about page. controller*/
+exports.about = function (req, res) {
+  res.render("about", { title: "About" });
+};
+
+/* GET services page. controller*/
+exports.services = function (req, res) {
+  res.render("services", { title: "Services" });
+};
+
+/* GET contact page. controller*/
+exports.contact = function (req, res) {
+  res.render("contact", { title: "Contact" });
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require("express");
+let path = require("path");
+let logger = require("morgan");
+
+let mainRoute = require("./routes/index");
+
+let app = express();
+let PORT = 3000;
+
+// For viewing the engine setup
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+// For the middlewares
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//For using the static files
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "./node_modules")));
+
+//For the routes
+app.use("/", mainRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
